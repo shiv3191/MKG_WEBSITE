3f20eda3c1cc52ef543d79fbea6d08fa3419f1ed
Shiv patel <130176300+shiv3191@users.noreply.github.com>
2025-04-09 10:35:26 +0530

    Update Distinction.jsx

--- src/Components/Distinctions/Distinction.jsx
+++ src/Components/Distinctions/Distinction.jsx
@@ -3,103 +3,57 @@ import "./Distinction.css"
 
 const Distinction = () => {
   return (
-    <di className='distinction'>
-      <div className='dist1'>
-        <p>
-         Honors/Awards/Recognition
-        </p>
+    <div className="distinction-container mt100">
+      <div className="heading-container">
+        <h1 className="distinction-heading">Honors/Awards/Recognition</h1>
       </div>
-       <div className='dist2'>
-        <p>
-        &#9632;   &nbsp;<b>Chair Professor</b>- BIS Standardization
-        </p>
-        <p>
-        &#9632;  &nbsp;Ranked in <b>Top 2% scientist</b> in list prepared by <b>Stanford University</b> and among the top 500 of the world's more than 42000 researchers in the in the sub-area "environmental engineering (Meteorology and Atmospheric Sciences)- 2020, 2021, 2022 and 2023
-        </p>
-        <p>
-        &#9632;  &nbsp;Prof S N Gupta Memorial Lecture Award, Indian Society of Hydraulics, 2021
-        </p>
-        <p>
-        &#9632;  &nbsp;Associate Editor-Scientific Reports, Nature Publishing Group
-        </p>
-        <p>
-        &#9632;   &nbsp;Associate Editor-International Journal of Cloud Applications and Computing
-        </p>
-        <p>
-        &#9632;  &nbsp;Best Researcher of the Year Award 2020 by Institution of Engineers (India)
-        </p>
-        <p>
-        &#9632;   &nbsp;Awarded American Society of Civil Engineers (ASCE’s)- Visiting International Fellowship 2019. 
-        </p>
-        <p>
-           &#9632;  &nbsp;Prof R J Garde Research Award
-        </p>
-        <p>
-        &#9632;   &nbsp;Associate Editor- ASCE- Journal of Hazardous, Toxic and Radioactive Waste
-        </p>
-        <p>
-        &#9632;  &nbsp;Best Theoretical-Oriented Paper Award in American Society of Civil Engineers (ASCE) 2018
-        </p>
-        <p>
-        &#9632;  &nbsp;IEI Young Engineers Award in Civil Engineering Division
-        </p>
-        <p>
-        &#9632;  &nbsp;Outstanding young scientist and awarded First Runner-Up prize during Indian Youth Poster Competition by Asia-Pacific Network for Global Change Research (APN), Japan 
-        </p>
-        <p>
-        &#9632;  &nbsp;BRNS Young Scientist Research Grant
-        </p>
-        <p>
-        &#9632; &nbsp;SGSITS National Award for Best Research Work by Young Teachers of Engineering Colleges
-        </p>
-        <p>
-        &#9632;   &nbsp;NIT Warangal Distinguished Young Alumni Professional Achievement Award
-        </p>
-        <p>
-        16. &nbsp;Indo-US WARI Fellowship Award
-        </p>
-        <p>
-        &#9632;  &nbsp;DST Young Scientist-Fast Track Grant
-        </p>
-        <p>
-        &#9632; &nbsp;Chairman and Editor-International conference on Residual Science and Environment
-        </p>
-        <p>
-        &#9632; &nbsp;INSPIRE Faculty Fellowship Award, India
-        </p>
-        <p>
-        &#9632; &nbsp;Erasmus Mundus Interweave Award, Europe 
-        </p>
-        <p>
-        &#9632; &nbsp;JSPS Fellowship Award, University of Tokyo, Tokyo, Japan
-        </p>
-        <p>
-        &#9632; &nbsp;Travel Fellowship Award, Workshop on Climate Informatics, NCAR, Boulder, Colorado, USA
-        </p>
-        <p>
-        &#9632; &nbsp;Postdoctoral Fellowship Award, McGill University, Canada
-        </p>
-        <p>
-        &#9632; &nbsp;Young Scientist Award, APEC Climate Center, Busan, Republic of Korea
-        </p>
-        <p>
-        &#9632; &nbsp;Postdoctoral Fellowship, Nanyang Technological University, Singapore
-        </p>
-        <p>
-        &#9632; &nbsp;Financial assistance to attend workshop at ICTP, Itly, Europe
-        </p>
-        <p>
-        &#9632; &nbsp;Travel Fellowship Award, CCCPR, Seoul, Republic of  Korea
-        </p>
-        <p>
-        &#9632; &nbsp;Postdoctoral Fellowship Award, University of California, San Digeo, USA
-        </p>
-        <p>
-        &#9632; &nbsp;Canadian Commonwealth Scholarship Award, University of Waterloo, Canada
-        </p>
-       </div>
-
-      </di>
+      <div className="content-container">
+        <div className="distinction-section">
+          <ul>
+            <li><b>Chair Professor</b> - BIS Standardization</li>
+            <li>Ranked in <b>Top 2% scientist</b> in list prepared by <b>Stanford University</b> and among the top 500 of the world's more than 42000 researchers in the in the sub-area "environmental engineering (Meteorology and Atmospheric Sciences) - 2020, 2021, 2022, 2023 and 2024.</li>
+            <li>Editor, PLOS-Climate.</li>
+            <li>Prof S N Gupta Memorial Lecture Award, Indian Society of Hydraulics, 2021</li>
+            <li>Associate Editor - Scientific Reports, Nature Publishing Group</li>
+            <li>Associate Editor - International Journal of Cloud Applications and Computing</li>
+            <li>Best Researcher of the Year Award 2020 by Institution of Engineers (India)</li>
+            <li>Young Researcher Award 2020, RSRI.</li>
+            <li>Awarded American Society of Civil Engineers (ASCE's) - Visiting International Fellowship 2019.</li>
+            <li>Prof R J Garde Research Award</li>
+            <li>Associate Editor - ASCE - Journal of Hazardous, Toxic and Radioactive Waste</li>
+            <li>Best Theoretical-Oriented Paper Award in American Society of Civil Engineers (ASCE) 2018</li>
+            <li>Research Grant 2018, NVIDIA GPU.</li>
+            <li>International Scientific Committee member 2018, The Protection and Restoration XIV International Conference, Thessaloniki.</li>
+            <li>IEI Young Engineers Award in Civil Engineering Division</li>
+            <li>Outstanding young scientist and awarded First Runner-Up prize during Indian Youth Poster Competition by Asia-Pacific Network for Global Change Research (APN), Japan</li>
+            <li>BRNS Young Scientist Research Grant</li>
+            <li>Profession Engineer (International) 2017, Institution of Engineers (India)</li>
+            <li>Certificate of Excellence for outstanding achievement as a participant in the quiz "Sustainable Development and Land Degradation" 2017, United Nations Convention to Combat Desertification, (UNCCD)</li>
+            <li>Advisory board member 2017, National Conference on Emerging Trends in Water Resources and Environmental Engineering (ETWREE).</li>
+            <li>SGSITS National Award for Best Research Work by Young Teachers of Engineering Colleges</li>
+            <li>NIT Warangal Distinguished Young Alumni Professional Achievement Award</li>
+            <li>Provia Fellowship Award 2016, UNEP</li>
+            <li>Indo-US WARI Fellowship Award</li>
+            <li>Scientific Committee member 2016, 13th International Protection and Restoration of the Environment (PRE) Conference, Mykonos Island, Greece</li>
+            <li>DST Young Scientist - Fast Track Grant</li>
+            <li>Chairman and Editor - International conference on Residual Science and Environment</li>
+            <li>Science Tour Fellowship 2014, DAAD</li>
+            <li>INSPIRE Faculty Fellowship Award, India</li>
+            <li>Erasmus Mundus Interweave Award, Europe</li>
+            <li>JSPS Fellowship Award, University of Tokyo, Tokyo, Japan</li>
+            <li>Travel Fellowship Award, Workshop on Climate Informatics, NCAR, Boulder, Colorado, USA</li>
+            <li>Postdoctoral Fellowship Award, McGill University, Canada</li>
+            <li>Young Scientist Award, APEC Climate Center, Busan, Republic of Korea</li>
+            <li>Certificate of Accomplishment - Student Leadership Award 2011, Organizational & Human Development, University of Waterloo, Canada</li>
+            <li>Postdoctoral Fellowship, Nanyang Technological University, Singapore</li>
+            <li>Financial assistance to attend workshop at ICTP, Itly, Europe</li>
+            <li>Travel Fellowship Award, CCCPR, Seoul, Republic of Korea</li>
+            <li>Postdoctoral Fellowship Award, University of California, San Digeo, USA</li>
+            <li>Canadian Commonwealth Scholarship Award, University of Waterloo, Canada</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
